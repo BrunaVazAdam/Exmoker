6bd0dcc528f1b4757d5f659508b03c1fb6e06421
Bruna Adam <brunaadam54@gmail.com>
2021-06-06 15:17:46 -0300

    Notificação de bem-vindo.

--- app/src/main/java/br/com/ifsul/bruna/exmoker/PreMainActivity.java
+++ app/src/main/java/br/com/ifsul/bruna/exmoker/PreMainActivity.java
@@ -1,9 +1,17 @@
 package br.com.ifsul.bruna.exmoker;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import br.com.ifsul.bruna.exmoker.colecoes.Tabagista;
 
@@ -30,6 +38,37 @@ public class PreMainActivity extends AppCompatActivity {
                 proximaTela = new Intent(PreMainActivity.this, MetodoParadaActivity.class);
             } else if (tabagista.getDataFinalizacaoCadastro() == null) {
                 estado.setFinalizacaoCadastro();
+
+                Intent intent = new Intent(this, MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+                NotificationCompat.Builder notificacao;
+                NotificationManagerCompat nm = NotificationManagerCompat.from(this);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    String idCanal = "WELCOME";
+                    NotificationChannel canal = new NotificationChannel(
+                            idCanal,
+                            "Canal para o recebimento de mensagem de boas vindas.",
+                            NotificationManager.IMPORTANCE_HIGH);
+                    nm.createNotificationChannel(canal);
+                    notificacao = new NotificationCompat.Builder(this, idCanal);
+                } else {
+                    notificacao = new NotificationCompat.Builder(this);
+                }
+
+                notificacao.setSmallIcon(R.drawable.ic_notificacao);
+                notificacao.setColor(getResources().getColor(R.color.exmoker_darker));
+                notificacao.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+                notificacao.setContentTitle("Bem vindo!");
+                notificacao.setContentText("Parabéns por tornar-se um Exmoker! \uD83C\uDF89");
+                notificacao.setPriority(NotificationCompat.PRIORITY_MAX);
+                notificacao.setAutoCancel(true);
+                notificacao.setContentIntent(pendingIntent);
+                notificacao.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+
+                nm.notify(1, notificacao.build());
+
                 proximaTela = new Intent(PreMainActivity.this, PreMainActivity.class);
             }
             startActivity(proximaTela);
